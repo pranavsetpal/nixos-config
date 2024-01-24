f744ec7b97580e5369831b09dfd8645a6f985af2
pranavsetpal <pranav.setpal@gmail.com>
2024-01-25 00:15:35 +0530

    switch kernel to latest

--- device/portable.nix
+++ device/portable.nix
@@ -3,10 +3,13 @@
     "${modulesPath}/installer/scan/not-detected.nix"
   ];
 
+  # boot.kernelPackages = pkgs.linuxPackages-libre;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [ "kvm-intel" "v4l2loopback" ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages =  with config.boot.kernelPackages; [];
 
   fileSystems."/boot/efi" = {
     device = "/dev/disk/by-label/BOOTEFI";
