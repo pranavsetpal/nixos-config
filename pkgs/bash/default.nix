366451b5a1be4de7298e61fa21da47f823d107ad
pranavsetpal <pranav.setpal@gmail.com>
2024-01-26 23:05:46 +0530

    add nixos-update aliases

--- pkgs/bash/default.nix
+++ pkgs/bash/default.nix
@@ -1,6 +1,27 @@
-{ lib, ... }: {
+{ lib, userInfo, ... }: {
   programs.bash = {
     enable = true;
-    bashrcExtra = lib.fileContents ./bashrc;
+    bashrcExtra = /* bash */ ''
+      export PS1="[\u@\H \W]\$ "
+
+      bind '"\e[A":history-search-backward'
+      bind '"\e[B":history-search-forward'
+
+      nixos-update() {
+          if [[ $1 = "sources" ]]; then
+              doas nix flake update ${userInfo.homedir}/.nixos/
+          elif [[ $1 = "os" ]]; then
+              doas nixos-rebuild switch --flake ${userInfo.homedir}/.nixos/#portable
+          elif [[ $1 = "home" ]]; then
+              home-manager switch --flake ${userInfo.homedir}/.nixos/
+          elif [[ $1 != "all" && $# != 0 ]]; then
+              echo "Unknown option: choose sources/os/home/all or leave blank for all"
+          else
+              doas nix flake update ${userInfo.homedir}/.nixos/
+              doas nixos-rebuild switch --flake ${userInfo.homedir}/.nixos/#portable
+              home-manager switch --flake ${userInfo.homedir}/.nixos/
+          fi
+      }
+    '';
   };
 }
