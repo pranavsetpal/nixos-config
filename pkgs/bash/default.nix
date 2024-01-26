256fb3b2f669ccc75921cc0ef81cddd151f6f3af
pranavsetpal <pranav.setpal@gmail.com>
2024-01-26 23:15:31 +0530

    make nixos-update 'if' logic simplier

--- pkgs/bash/default.nix
+++ pkgs/bash/default.nix
@@ -10,16 +10,20 @@
       nixos-update() {
           if [[ $1 = "sources" ]]; then
               doas nix flake update ${userInfo.homedir}/.nixos/
+
           elif [[ $1 = "os" ]]; then
               doas nixos-rebuild switch --flake ${userInfo.homedir}/.nixos/#portable
+
           elif [[ $1 = "home" ]]; then
               home-manager switch --flake ${userInfo.homedir}/.nixos/
-          elif [[ $1 != "all" && $# != 0 ]]; then
-              echo "Unknown option: choose sources/os/home/all or leave blank for all"
-          else
+
+          elif [[ $# == 0 || $1 == "all" ]]; then
               doas nix flake update ${userInfo.homedir}/.nixos/
               doas nixos-rebuild switch --flake ${userInfo.homedir}/.nixos/#portable
               home-manager switch --flake ${userInfo.homedir}/.nixos/
+
+          else
+              echo "Unknown option: choose sources/os/home/all or leave blank for all"
           fi
       }
     '';
