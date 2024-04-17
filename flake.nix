c87edc7c04397955c277e2a944aa261268e5b808
pranavsetpal <pranav.setpal@gmail.com>
2024-04-18 03:53:59 +0530

    remove nixos-stable input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "nixos setup flake";
 
   inputs = {
-    nixos-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
@@ -13,12 +12,11 @@
     zig-overlay.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixos-stable, nixos-unstable, home-manager, zig-overlay, ... }:
+  outputs = { self, nixos-unstable, home-manager, zig-overlay, ... }:
     let
       system = "x86_64-linux";
 
       pkgs = import nixos-unstable { inherit system; };
-      stable = import nixos-stable { inherit system; };
 
       userInfo = rec {
         name = "pranav";
